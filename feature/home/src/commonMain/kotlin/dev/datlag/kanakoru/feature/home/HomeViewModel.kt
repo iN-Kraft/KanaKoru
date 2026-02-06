32052e54aa6dc5da3195bb52a3db0a86122e7efd
Jeff Retz <jeff.retz08@gmail.com>
2026-02-06 19:39:54 +0100

    removed wrong greeting

--- feature/home/src/commonMain/kotlin/dev/datlag/kanakoru/feature/home/HomeViewModel.kt
+++ feature/home/src/commonMain/kotlin/dev/datlag/kanakoru/feature/home/HomeViewModel.kt
@@ -6,7 +6,6 @@ import dev.datlag.kanakoru.feature.home.resources.HomeRes
 import dev.datlag.kanakoru.feature.home.resources.greeting
 import dev.datlag.kanakoru.feature.home.resources.greeting_evening
 import dev.datlag.kanakoru.feature.home.resources.greeting_morning
-import dev.datlag.kanakoru.feature.home.resources.greeting_night
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -45,8 +44,7 @@ class HomeViewModel : ViewModel() {
         val message = when (currentHour) {
             in 5..11 -> HomeRes.string.greeting_morning
             in 12..16 -> HomeRes.string.greeting
-            in 17..21 -> HomeRes.string.greeting_evening
-            else -> HomeRes.string.greeting_night
+            else -> HomeRes.string.greeting_evening
         }
 
         _greeting.update { message }
